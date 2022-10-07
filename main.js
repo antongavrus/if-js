7dfbf0e1c6820be6b49c325ebe11f2f55859f704
antongavrus <76228197+antongavrus@users.noreply.github.com>
2022-10-07 08:37:06 +0300

    Lesson 3 (#4)
    * move or commit
    
    * function 1 + function 2
    
    * Removed node_modules

--- main.js
+++ main.js
@@ -1,62 +1,89 @@
-// 6. Работа с переменными
-
-let user = 'John Doe';
-console.log(user);
-
-const student = 'Anton';
-console.log(student);
-
-user = student;
-// Переменной user присвоилось значение student, то есть "Anton"
-console.log(user);
-
-// 7. Работа с примитивами
-console.log('----------------------');
-
-let test = 1;
-
-test += 1;
-// 2
-console.log(test);
-
-test -= 1;
-// 1
-console.log(test);
-
-test = true;
-// Значение в bool (true)
-console.log(test);
-
-// 8. Дан массив [2, 3, 5, 8].
-console.log('----------------------');
-
-let result = 1;
-const arr = [2, 3, 5, 8];
-
-for (let i = 0; i < arr.length; i++) {
-  result = result * arr[i];
-}
-
-console.log(result);
-
-// 9
-console.log('----------------------');
-
-const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < arr2.length; i++) {
-  if (arr2[i] > 5 && arr2[i] < 10) {
-    console.log(arr2[i]);
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else b < a;
+  {
+    return b;
   }
 }
 
-// 10
-console.log('----------------------');
+console.log(min(1, 90));
 
-const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < arr3.length; i++) {
-  if (arr3[i] % 2 === 0) {
-    console.log(arr3[i]);
+function max(a, b) {
+  if (a > b) {
+    return a;
+  } else b > a;
+  {
+    return b;
   }
 }
+
+console.log(max(50, 40));
+
+console.log('----------------------');
+console.log('----------------------');
+console.log('----------------------');
+
+const ar = [2, 12, 100, 44, 50, 77, 80, 90, 15, 1];
+
+function replace() {
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i] % 100 === 0) {
+      ar[i] = ar[i] / 100 + 'zerozero';
+    } else if (ar[i] % 10 === 0) {
+      ar[i] = ar[i] / 10 + 'zero';
+    }
+  }
+  return ar;
+}
+
+console.log(replace(ar));
+
+console.log('----------------------');
+console.log('----------------------');
+console.log('----------------------');
+
+function reverseString(str) {
+  let reverse = '';
+  for (const i = str.length - 1; i >= 0;) {
+    reverse += str[i];
+  }
+  return reverse;
+}
+
+console.log(reverseString('qwwq'));
+
+console.log('----------------------');
+console.log('----------------------');
+console.log('----------------------');
+
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+console.log(sum(5)(2));
+
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+function qwerty() {
+  let i = 0;
+  return function (event) {
+    event.target.style.color = colors[i];
+    i++;
+    if (i > colors.length) {
+      i = 0;
+    }
+  };
+}
+
+text1.addEventListener('click', qwerty());
+
+text2.addEventListener('click', qwerty());
+
+text3.addEventListener('click', qwerty());
