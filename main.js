5f8a7ab53494390911b8905d1ca8f48a75cb4be1
antongavrus <76228197+antongavrus@users.noreply.github.com>
2022-10-10 17:24:34 +0300

    fixed

--- main.js
+++ main.js
@@ -1,55 +1,57 @@
-const re = new RegExp("^(\\d{1,2})\-(\\d{1,2})\-(\\d{4})$");
+const re = new RegExp('^(\\d{1,2})-(\\d{1,2})-(\\d{4})$');
 const str = '11-13-2016';
 const newStr = str.replace(re, '$3.$2.$1');
 console.log(newStr);
 
 const data = [
-    {
-        country: 'Russia',
-        city: 'Saint Petersburg',
-        hotel: 'Hotel Leopold',
-    },
-    {
-        country: 'Spain',
-        city: 'Santa Cruz de Tenerife',
-        hotel: 'Apartment Sunshine',
-    },
-    {
-        country: 'Slowakia',
-        city: 'Vysokie Tatry',
-        hotel: 'Villa Kunerad',
-    },
-    {
-        country: 'Germany',
-        city: 'Berlin',
-        hotel: 'Hostel Friendship',
-    },
-    {
-        country: 'Indonesia',
-        city: 'Bali',
-        hotel: 'Ubud Bali Resort&SPA',
-    },
-    {
-        country: 'Netherlands',
-        city: 'Rotterdam',
-        hotel: 'King Kong Hostel',
-    },
-    {
-        country: 'Marocco',
-        city: 'Ourika',
-        hotel: 'Rokoko Hotel',
-    },
-    {
-        country: 'Germany',
-        city: 'Berlin',
-        hotel: 'Hotel Rehberge Berlin Mitte',
-    },
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
 ];
 
-
 function getObjectData(word) {
-     return data.forEach((element) =>
-         element.country.includes(word) || element.city.includes(word) || element.hotel.includes(word))
+  return data.filter(
+      (element) => {
+        if (element.country.includes(word) || element.city.includes(word) || element.hotel.includes(word)) {
+          return element
+        }
+      })
 }
-
-console.log(getObjectData('Berlin'))
+console.log(getObjectData('Russia'));
